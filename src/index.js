fa364612069d4bed948db7a6c333dba33ff09d9f
Amaresh  S M <30730124+amareshsm@users.noreply.github.com>
2020-04-23 23:23:39 +0530

    landing page added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Landing from './Landing'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Landing/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
